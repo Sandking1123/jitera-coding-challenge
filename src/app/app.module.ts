1c07f74f3f7271291c13e565015e3da070061c7a
sandking1123 <lucas0holm@gmail.com>
2023-05-22 04:16:58 +0300

    Task3 complete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -19,8 +20,9 @@ import { TravelOptionsComponent } from './travel-options/travel-options.componen
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/travel-options/travel-options.component.ts
+++ src/app/travel-options/travel-options.component.ts
@@ -1,10 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+interface OptionType {
+  name: string;
+  pricePerPassenger: number;
+  vehicleType: {
+    name: string;
+    maxPassengers: number;
+  }
+}
 
 @Component({
   selector: 'app-travel-options',
   templateUrl: './travel-options.component.html',
   styleUrls: ['./travel-options.component.scss']
 })
-export class TravelOptionsComponent {
+export class TravelOptionsComponent implements OnInit {
+  travelOptions: any;
+  optionListings: OptionType[] = [];
+  displayOptionListings: OptionType[] = [];
+  totalListings: number = 0;
+  maxPrice: number = 0;
+  minPrice: number = 0;
+  avgPrice: number = 0;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.getTravelOptions();
+  }
+
+  getTravelOptions() {
+    const url = '/api/QuoteRequest';
+    return this.http.get(url).subscribe(response => {
+      this.travelOptions = response as OptionType[];
+      this.optionListings = this.travelOptions.listings
+        .filter((option: OptionType) => { return option.vehicleType.maxPassengers > 2; })
+        .sort((a: OptionType, b: OptionType) => a.pricePerPassenger - b.pricePerPassenger);
+      this.totalListings = this.optionListings.length;
+      this.maxPrice = Math.max(...this.optionListings.map((option: OptionType) => option.pricePerPassenger));
+      this.minPrice = Math.min(...this.optionListings.map((option: OptionType) => option.pricePerPassenger));
+      this.avgPrice = this.calculateAveragePrice();
+    });
+  }
+
+  calculateAveragePrice(): number {
+    const sum = this.optionListings.reduce((total: number, option: OptionType) => total + option.pricePerPassenger, 0);
+    const result = sum / this.totalListings;
+    return Number(parseFloat(result.toString()).toFixed(3));
+  }
 
 }
